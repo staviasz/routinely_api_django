b5bc389e76f3fbbadab9690403574019208c5ce5
staviasz <erickesc21@hotmail.com>
2024-10-06 13:48:08 -0300

    feat: password value object

---
+++ src/modules/customer/domain/aggregate/value_objects/__tests__/password_value_object_test.py
@@ -0,0 +1,35 @@
+import pytest
+
+from main.errors.shared import CustomError
+from modules.customer.domain.aggregate.value_objects import PasswordValueObject
+
+
+class TestPasswordValueObject:
+    def test_raise_error(self):
+        with pytest.raises(CustomError) as error:
+            PasswordValueObject("")
+
+        custom_error = error.value
+        assert custom_error.formated_errors == {
+            "code_error": 400,
+            "messages_error": [
+                "The password is required.",
+                "The password must contain at least one uppercase letter, one lowercase letter, one special character, one number and be at least 6 characters long",
+            ],
+        }
+
+    def test_password_invalid(self):
+        with pytest.raises(CustomError) as error:
+            PasswordValueObject("Teste1")
+
+        custom_error = error.value
+        assert custom_error.formated_errors == {
+            "code_error": 400,
+            "messages_error": [
+                "The password must contain at least one uppercase letter, one lowercase letter, one special character, one number and be at least 6 characters long",
+            ],
+        }
+
+    def test_password_valid(self):
+        vo = PasswordValueObject("@Teste123")
+        assert vo.value == "@Teste123"
---
+++ src/modules/customer/domain/aggregate/value_objects/password_value_object.py
@@ -0,0 +1,29 @@
+import re
+from main.domain.value_objects.value_objects import ValueObject
+from main.errors.domain import FieldIsRequiredError
+from modules.customer.domain.errors import InvalidPasswordError
+
+
+class PasswordValueObject(ValueObject[str]):
+    def __init__(self, password: str) -> None:
+        self._validate(password)
+        super().__init__(password)
+
+    def _validate(self, password: str) -> None:
+        self._clear_errors()
+
+        self.__has_password(password)
+        self.__is_valid_password(password)
+
+        self._raize_errors()
+
+    def __has_password(self, password: str) -> None:
+        if not password:
+            self._add_error(FieldIsRequiredError("password"))
+
+    def __is_valid_password(self, password: str) -> None:
+        password_regex = (
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*=])[a-zA-Z\d!@#$%&*=]{6,}$"
+        )
+        if not re.match(password_regex, password):
+            self._add_error(InvalidPasswordError())
