2b2253100406942d73d46cb5ea14baca4cde608b
staviasz <erickesc21@hotmail.com>
2024-10-06 20:16:52 -0300

    chore: pre-commit execute pytest

--- src/modules/customer/domain/errors/invalid_name_error.py
+++ src/modules/customer/domain/errors/invalid_name_error.py
@@ -5,5 +5,5 @@ class InvalidNameError(CustomErrorAbstract):
     def __init__(self) -> None:
         super().__init__(
             code_error=400,
-            message_error=f"The name must be between 3 and 70 characters and only letters",
+            message_error="The name must be between 3 and 70 characters and only letters",
         )
